07f2d058b59bd427eae7c0f87fde0c2a0bef3627
Maurício Camacho <joaocamacho91@gmail.com>
2023-05-01 18:28:21 +0100

    build client insert is name before connect with the server

--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class MainClient {
 
     public static void main ( String[] args ) throws Exception {
-        Client client = new Client ( 8000 );
+
+        Scanner usrInput = new Scanner ( System.in );
+        System.out.println ( "Insert your name:" );
+        String userName = usrInput.nextLine ( );
+
+        Client client = new Client ( 8000 , userName);
         client.execute ( );
     }
 
