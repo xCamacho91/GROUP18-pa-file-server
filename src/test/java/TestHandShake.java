e89ca1d006a6b51c6d451e0a6d22278fd3c890c3
cenamaluco <jpfranquinhog@gmail.com>
2023-05-08 02:58:55 +0100

    test : junit test for handshake and gitactions

--- src/test/java/TestHandShake.java
+++ src/test/java/TestHandShake.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -12,5 +13,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestHandShake {
     @Test
     public void testRunMethod() throws Exception {
+        //initialize RSA keys
+        KeyPair keyPair = Encryption.generateKeyPair ( );
+        PublicKey serverPublicRSAKey = keyPair.getPublic();
+        PrivateKey serverPrivateRSAKey = keyPair.getPrivate();
+
+        keyPair = Encryption.generateKeyPair ( );
+        PublicKey clientPublicRSAKey = keyPair.getPublic();
+        PrivateKey clientPrivateRSAKey = keyPair.getPrivate();
+
+        //Diffiehellman
+            //server
+                // Generate a pair of keys
+        BigInteger serverPrivateDHKey = DiffieHellman.generatePrivateKey ( );
+        BigInteger serverPublicDHKey = DiffieHellman.generatePublicKey ( serverPrivateDHKey );
+        byte[] encryptedServerPublicDHKey = Encryption.encryptRSA ( serverPublicDHKey.toByteArray ( ) , serverPrivateRSAKey );
+
+            //client
+                // Generate a pair of keys
+        BigInteger clientPrivateDHKey = DiffieHellman.generatePrivateKey ( );
+        BigInteger clientPublicDHKey = DiffieHellman.generatePublicKey ( clientPrivateDHKey );
+        byte[] encryptedClientPublicDHKey = Encryption.encryptRSA ( clientPublicDHKey.toByteArray ( ) , clientPrivateRSAKey );
+
+            //generate sharedKeys
+
+        BigInteger decryptedClientPublicDHKey = new BigInteger ( Encryption.decryptRSA ( encryptedClientPublicDHKey , clientPublicRSAKey ) );
+        BigInteger serverSharedKey = DiffieHellman.computePrivateKey ( decryptedClientPublicDHKey , serverPrivateDHKey );
+
+        BigInteger decryptedServerPublicDHKey = new BigInteger ( Encryption.decryptRSA ( encryptedServerPublicDHKey , serverPublicRSAKey ) );
+        BigInteger clientSharedKey = DiffieHellman.computePrivateKey ( decryptedServerPublicDHKey , clientPrivateDHKey );
+
+        assertEquals(serverSharedKey,clientSharedKey);
     }
 }
